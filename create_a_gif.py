31d4838f48376712a12b08f0c33c1bbfcc1c53bf
Riley <160427476+rileycsv@users.noreply.github.com>
2025-06-24 21:19:32 -0400

    Delete create_a_gif.py

--- create_a_gif.py
+++
@@ -1,9 +0,0 @@
-import imageio.v3 as iio
-
-filenames = ['duck1.png', 'duck2.png']
-images = [ ]
-
-for filename in filenames:
-  images.append(iio.imread(filename))
-
-iio.imwrite('duck.gif', images, duration = 500, loop = 0)
